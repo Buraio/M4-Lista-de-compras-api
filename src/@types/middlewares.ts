491c07a24b7c70ebd460d039263d1c36deb79c7d
Bryan <bryanbatista79688@gmail.com>
2023-01-27 18:52:43 -0300

    feat: post entry validation for createList

--- src/@types/middlewares.ts
+++ src/@types/middlewares.ts
@@ -1,5 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import { globalProductListDatabase } from "./apiLogic";
+import { iProduct, requiredDataKeys, requiredKeys } from "./types";
+
+const requiredKeys: requiredKeys = ["listName", "data"];
+const requiredDataKeys: requiredDataKeys = ["name", "quantity"];
 
 export const ensureProductListExists = (
   request: Request,
@@ -15,6 +19,54 @@ export const ensureProductListExists = (
   if (productListId === -1) {
     return response.status(404).send({
       message: `List with Id '${paramsId}' does not exist`,
+    });
+  }
+
+  next();
+};
+
+export const ensureCreateRequestIsOk = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Response | void => {
+  const requestKeys = Object.keys(request.body);
+  const requestData = request.body.data;
+
+  const requestValues = Object.values(request.body);
+  console.log(requestValues)
+  const validateTypes = requestValues.every((key) => {
+    console.log(key)
+    if (typeof key !== "string" || !Array.isArray(key)) {
+      console.log('pão')
+      // return response.status(400).json({
+      //   message: `Properties 'listName', 'name' and 'quantity' need to be a string`,
+      // })
+    }
+  });
+
+  const validateKeys = requiredKeys.every((key) => {
+    if (requestKeys.includes(key)) {
+      return requestKeys.length === requiredKeys.length;
+    }
+  });
+
+  const validateDataKeys = requestData.every((product: iProduct) => {
+    const requestItemKeys = Object.keys(product);
+    const validation = requiredDataKeys.every((key) => {
+        if (requestItemKeys.includes(key)) {
+          return requestItemKeys.length === requiredDataKeys.length;
+        }
+      }
+    );
+
+    return validation;
+  });
+
+  if (!validateKeys || !validateDataKeys) {
+    return response.status(400).json({
+      message:
+        "Only accepted properties are 'listName', 'data', 'name' and 'quantity'",
     });
   }
 
@@ -58,8 +110,7 @@ export const ensureUpdateRequestIsOk = (
   }
 
   const requestKeys = Object.keys(request.body);
-  const requiredKeys = ["name", "quantity"];
-  const validateKeys: boolean = requiredKeys.every((key) =>
+  const validateKeys: boolean = requiredDataKeys.every((key) =>
     requestKeys.includes(key)
   );
 
--- src/@types/types.ts
+++ src/@types/types.ts
@@ -8,3 +8,7 @@ export interface iProduct {
   name: string;
   quantity: string;
 }
+
+export type requiredKeys = ["listName", "data"];
+
+export type requiredDataKeys = ["name", "quantity"];
